03e016c3d7909ffd2c162ec21dff61076442fa05
Andrew Waterman <waterman@s141.Millennium.Berkeley.EDU>
2010-09-09 15:39:40 -0700

    [pk, sim] added interrupt support to sim; added timer interrupt

--- pk/pcr.h
+++ pk/pcr.h
@@ -8,17 +8,21 @@
 #define SR_S     0x0000000000000008
 #define SR_EF    0x0000000000000010
 #define SR_UX    0x0000000000000020
-#define SR_KX    0x0000000000000040
+#define SR_SX    0x0000000000000040
 #define SR_IM    0x000000000000FF00
 
 #define PCR_SR       0
 #define PCR_EPC      1
 #define PCR_BADVADDR 2
 #define PCR_TBR      3
+#define PCR_COUNT    4
+#define PCR_COMPARE  5
 #define PCR_TOHOST   16
 #define PCR_FROMHOST 17
 #define PCR_K0       24
 #define PCR_K1       25
+
+#define TIMER_PERIOD 0x1000000
 
 #define ASM_CR(r)   _ASM_CR(r)
 #define _ASM_CR(r)  $cr##r
--- pk/riscv-pk.c
+++ pk/riscv-pk.c
@@ -5,18 +5,18 @@ void __attribute__((section(".boottext"))) __start()
   extern char stack_top;
   asm("move $sp,%0" : : "r"(&stack_top-64));
 
-  register long sr0 = SR_S | SR_PS | SR_ET;
+  extern char trap_table;
+  register void* tt = &trap_table;
+  mtpcr(tt,PCR_TBR);
+
+  register long sr0 = SR_S | SR_PS | SR_ET | SR_IM;
   #ifdef PK_ENABLE_KERNEL_64BIT
-    sr0 |= SR_KX;
+    sr0 |= SR_SX;
     #ifdef PK_ENABLE_USER_64BIT
       sr0 |= SR_UX;
     #endif
   #endif
   mtpcr(sr0,PCR_SR);
-
-  extern char trap_table;
-  register void* tt = &trap_table;
-  mtpcr(tt,PCR_TBR);
 
   extern void boot();
   register void (*boot_p)() = &boot;

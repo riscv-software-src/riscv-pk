e63e4fbe8794c68e1ad846a36e33cba62e801b34
Andrew Waterman <waterman@s141.Millennium.Berkeley.EDU>
2011-06-19 20:37:58 -0700

    Renamed packages

--- pk/frontend.h
+++
@@ -1,8 +0,0 @@
-#ifndef _RISCV_FRONTEND_H
-#define _RISCV_FRONTEND_H
-
-#include <machine/syscall.h>
-
-sysret_t frontend_syscall(long n, long a0, long a1, long a2, long a3);
-
-#endif
--- pk/syscall.h
+++
@@ -1,2 +0,0 @@
-
-#include <machine/syscall.h>

9037b1d9f2130770228be62c66b88b47fccd99b6
Palmer Dabbelt <palmer@dabbelt.com>
2017-07-27 13:23:49 -0700

    Add a platform interface
    SiFive's pk fork is the second one I've had to maintain, and it's a huge
    pain because people keep just leaving changes all over the tree.  I want
    to introduce an interface that the platform-specific details can live
    behind so I don't have to keep doing these painful merges.

---
+++ platform/platform_interface.h
@@ -0,0 +1,14 @@
+#ifndef PLATFORM__PLATFORM_H
+#define PLATFORM__PLATFORM_H
+
+/* This interface is designed to allow BBL/PK to be portable to multiple target
+ * platforms.  The current interface has been hacked up based on SiFive's fork
+ * of pk that runs on our FPGA boards.  The idea here is that rather than
+ * forking pk and touching things all over the tree, changes should be local to
+ * the target directory.
+ *
+ * This interface isn't meant to be stable or sane, just better than what we
+ * had before.
+ */
+
+#endif
---
+++ platform/spike.c
@@ -0,0 +1,1 @@
+#include "platform_interface.h"

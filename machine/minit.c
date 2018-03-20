5a0e3e55cab1d6f3462c77e852f16365c79f2c98
Wesley W. Terpstra <wesley@sifive.com>
2018-03-20 12:54:40 -0700

    minit: insert printm as work-around for a race condition
    Reboot does not work reliably without this.

--- machine/minit.c
+++ machine/minit.c
@@ -143,6 +143,7 @@ void init_first_hart(uintptr_t hartid, uintptr_t dtb)
   query_uart(dtb);
   query_uart16550(dtb);
   query_htif(dtb);
+  printm("bbl loader\r\n");
 
   hart_init();
   hls_init(0); // this might get called again from parse_config_string
